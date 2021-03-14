34d2a5b6a5d789b761ece85e5e3f8df8450b9d8d
norman-andrians <65523690+norman-andrians@users.noreply.github.com>
2021-03-14 16:26:53 +0700

    added rickroll
    pfft i'm just kidding :stuck_out_tongue_closed_eyes:

---
+++ secret-documents/script.js
@@ -0,0 +1,7 @@
+function clicked() {
+    var choose = confirm("are you f*cking sure about that?");
+
+    if (choose == true) {
+        window.location.href = "https://youtu.be/iik25wqluFo";
+    }
+}
