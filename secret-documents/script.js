14743f61bf0d3b6031d6f69c887c283531a1d325
norman-andrians <65523690+norman-andrians@users.noreply.github.com>
2021-03-14 16:39:39 +0700

    update link
    sh*t wrong link

--- secret-documents/script.js
+++ secret-documents/script.js
@@ -2,6 +2,6 @@ function clicked() {
     var choose = confirm("are you f*cking sure about that?");
 
     if (choose == true) {
-        window.location.href = "https://youtu.be/iik25wqluFo";
+        window.location.href = "https://youtu.be/iik25wqIuFo";
     }
 }
