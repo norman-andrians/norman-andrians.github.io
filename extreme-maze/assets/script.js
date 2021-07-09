83f0e49583d21519f9763b20971dd52fdfa1b927
norman-andrians <norman25.na@gmail.com>
2021-07-09 20:06:55 +0700

    change device detect

--- extreme-maze/assets/script.js
+++ extreme-maze/assets/script.js
@@ -7,9 +7,18 @@ window.onload = function() {
         if (navigator.userAgent.match(/Windows/i)) {
         	window.location.href = './assets/files/extreme-maze-3d-0.44-beta-windows-64x.zip';
         	downloadclick.download = 'extreme-maze-3d-0.44-beta-windows-64x.zip';
-        } else {
+        } else if (navigator.userAgent.match(/Mac Os/i)) {
+        	alert("I'm sorry but you can only download for Windows");
+        	alert("but don't worry, we will make it for Mac Os ;)");
+        } else if (navigator.userAgent.match(/Linux/i)) {
+        	alert("I'm sorry but you can only download for Windows");
+        	alert("but don't worry, we will make it for Linux ;)");
+        } else if (navigator.userAgent.match(/Android/i)) {
         	alert("I'm sorry but you can only download for windows");
         	alert("but don't worry, we will make it for Android ;)");
+        } else {
+        	alert("I'm sorry but you can only download for windows");
+        	alert("but don't worry, we will make it for your Device ;)");
         }
     });
 };
